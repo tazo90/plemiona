ab9c00e50f9f4caa6c1ac4ec7a8b9c78d0f951ca
Mariusz Winnik <m.winnik@dook.pl>
2015-03-23 23:12:24 +0100

    Update settings.py

--- lpp/settings.py
+++ lpp/settings.py
@@ -16,7 +16,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.        
         'NAME': 'lpp_proj',                      # Or path to database file if using sqlite3.
         'USER': 'django',                      # Not used with sqlite3.
-        'PASSWORD': 'pchela',                  # Not used with sqlite3.
+        'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
